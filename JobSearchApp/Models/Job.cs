627fda203b6b923a2af41c83167e299b9d3da21f
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-12-22 15:14:30 +0630

    Add project files.

---
+++ JobSearchApp/Models/Job.cs
@@ -0,0 +1,17 @@
+﻿namespace JobSearchApp.Models
+{
+    public class Job
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Company { get; set; }
+        public string CompanyUrl { get; set; }
+        public string Location { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string HowToApply { get; set; }
+        public string CompanyLogo { get; set; }
+    }
+}
